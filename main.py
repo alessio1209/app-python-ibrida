31c2c21ab487c0da91d3ce5b422dd097b85bc6b5
Alessio1209 <alessioercolano2@gmail.com>
2026-04-16 11:56:45 +0200

    fix: risolto problema nel file main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI #libreria principale per creare l'infrastruttura del server web
 from fastapi.responses import FileResponse #importiamo questo strumento per permette a python di prendere un file fisico (il nostro html) e spedirlo al browser del client
-import random #libreria standard di python per generare numeri casuali
+import secrets #libreria di python per generare numeri o stringhe crittografate
 
 app = FastAPI(title="IoT Textile Monitor") #inizializziamo la nostra applicazione. Questa variabile app è il cuore del nostro server
 
@@ -10,8 +10,8 @@ def serve_frontend(): #funzione che gestisce la richiesta iniziale
 
 @app.get("/api/telai") #creiamo un nuovo indirizzo (Endpoint API). Questo non dà una pagina visiva, ma dà dati che il frontend leggerà 
 def get_dati_telai(): #funzione che calcola i dati in tempo reale
-	efficienza = random.randint(75,100) 
-	temperatura = random.randint(60,90)
+	efficienza = secrets.choice(range(75,101)) 
+	temperatura = secrets.choice(range(60,91))
 	#se la temperatura supera gli 85 gradi, mandiamo un allert
 	stato = "ALLARME TEMPERATURA" if temperatura > 85 else "ATTIVO"
 	#restituiamo un dizionario (che FastAPI trasformerà in automatico in un perfetto JSON)
